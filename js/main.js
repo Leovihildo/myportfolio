31c66127760b7e24cdbc36f1370d9d32b0e0a3f9
Leovihildo <46786869+Leovihildo@users.noreply.github.com>
2022-12-17 09:59:44 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -180,10 +180,21 @@ let swiper = new Swiper(".mySwiper", {
     mousewheel: true,
     keyboard: true,
 });
-var swiper = new Swiper(".mySwiper2", {
+var swiper1 = new Swiper(".mySwiper1", {
         loop: true,
         spaceBetween: 10,
         slidesPerView: 4,
         freeMode: true,
         watchSlidesProgress: true,
+      });
+var swiper2 = new Swiper(".mySwiper2", {
+        loop: true,
+        spaceBetween: 10,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        thumbs: {
+          swiper: swiper,
+        },
       });
